b8fec277806130b602e214d39eeccf8089626b47
LisaJean-philippe <lj67621n@pace.edu>
2025-07-02 08:34:03 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Fan from './fan';
+
+function App() {
+  return (
+    <div>
+      <h1>Dynamic Fan Graphics</h1>
+      <Fan />
+    </div>
+  );
+}
+
+export default App;
